14423306e6ebd9a0c356b710380327242519d027
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-11-06 21:07:10 +0900

    Config routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Zoomix::Application.routes.draw do
+  controller :sessions do
+    get 'auth/:provider/callback' => :callback
+    delete 'logout' => :destroy
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -49,6 +53,7 @@ Zoomix::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  root to: 'home#index', constraints: -> request { request.session[:user_id].present? }, as: 'home'
   root to: 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
