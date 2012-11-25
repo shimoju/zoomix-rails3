88f13ba2799e1b20888b25263a7093957b33467c
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-11-25 20:51:50 +0900

    Add current_user() and Use it for authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authorize
 
+  helper_method :current_user
+
   private
   def authorize
-    unless User.find_by_id(session[:user_id])
+    unless current_user
       session[:user_id] = nil
       redirect_to root_url, notice: "ログインしてください"
     end
+  end
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 end
