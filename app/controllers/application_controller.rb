cfe961546f7be276f65e29bcdfa10ce12884343f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-12-08 22:10:59 +0900

    Prepare i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV["BASIC_AUTH_NAME"], password: ENV["BASIC_AUTH_PASSWORD"] if Rails.env.production?
   protect_from_forgery
+
+  before_filter :set_locale
+
+  private
+    def set_locale
+      I18n.locale = session[:locale] || I18n.default_locale
+    end
 end
