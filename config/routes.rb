f3b09de0b8d2456a19179a1f93003e53ae7b3207
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-12-08 20:27:47 +0900

    Install Active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Zoomix::Application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
