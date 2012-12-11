c8349ff29a7f1af4c1f1b7401f3e9b12992804a7
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-12-11 21:16:34 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Zoomix::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   get 'about' => 'welcome#about'
