54cc6f093fe3ff28927918780bc68a1f2dd3fb04
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2012-12-12 00:02:03 +0900

    Change OmniauthCallbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env['omniauth.auth']
     @user = User.from_omniauth(auth)
 
-    sign_in_and_redirect @user
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+    else
+      redirect_to root_url
+      set_flash_message(:alert, :failure, kind: 'Twitter', reason: @user.errors.full_messages.join(''))
+    end
   end
 end
